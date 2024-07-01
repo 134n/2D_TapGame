e74614167eb39370bef77264c950597ab3ae88f9
134n <goruji.ruji@gmail.com>
2024-07-01 22:09:39 +0900

    Clean : GameManager.csのリファクタリング(誤字、命名のわかりやすさ、機能変更はなし、コードの整理)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,53 +2,62 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject purplePointPrefub;
+    [SerializeField] GameObject purplePointPrefab;
     [SerializeField] Camera cam;
+    private TouchObjectGenerator touchObjectGenerator;
 
     private void Start()
     {
-        ObjectSpawn objectSpawn = new(purplePointPrefub, cam);
-
-        objectSpawn.Spawn();
+        touchObjectGenerator = new(purplePointPrefab, cam);
+        touchObjectGenerator.GenerateObject();
     }
 
     private void Update()
     {
-        ObjectSpawn objectSpawn = new(purplePointPrefub, cam);
-
-        objectSpawn.MouseDetection();
+        touchObjectGenerator.IsClickedObject();
+        touchObjectGenerator.ReGenerateObject();
     }
 
-    private class ObjectSpawn
+    private class TouchObjectGenerator
     {
-        private readonly GameObject purplePointPrefub;
+        private readonly GameObject purplePointPrefab;
         private readonly Camera cam;
+        private bool isClickedObject;
+        private GameObject hitObject;
 
-        public ObjectSpawn(GameObject purplePointPrefub, Camera cam)
+        public TouchObjectGenerator(GameObject purplePointPrefab, Camera cam)
         {
-            this.purplePointPrefub = purplePointPrefub;
+            this.purplePointPrefab = purplePointPrefab;
             this.cam = cam;
         }
 
-        public void Spawn()
+        public void GenerateObject()
         {
             Vector3 spawn = new(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), 0);
-            Instantiate(purplePointPrefub, spawn, Quaternion.identity);
+            Instantiate(purplePointPrefab, spawn, Quaternion.identity);
         }
-        
-        public void MouseDetection()
+
+        public void IsClickedObject()
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10f);
 
             if (!Input.GetMouseButtonDown(0)) return;
-            {
-                if (!hit.collider) return;
-                {
-                    Destroy(hit.collider.gameObject);
-                    Spawn();
-                }
-            }
+            if (!hit.collider) return;
+
+            isClickedObject = true;
+            hitObject = hit.collider.gameObject;
+        }
+
+        public void ReGenerateObject()
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            if (!hitObject) return;
+            if (!isClickedObject) return;
+
+            Destroy(hitObject);
+            GenerateObject();
+
         }
     }
 }
