dde5a1bcb25c8f543bc55d5bc71ffdb1b1981c14
134n <goruji.ruji@gmail.com>
2024-07-07 16:05:23 +0900

    clean : メソッド名誤字

--- Assets/Scripts/TouchObjectGenerator.cs
+++ Assets/Scripts/TouchObjectGenerator.cs
@@ -3,26 +3,19 @@ using UnityEngine;
 public class TouchObjectGenerator : MonoBehaviour
 {
     [SerializeField] GameObject purplePointPrefab;
-    private readonly bool isClickedObject;
-    private readonly GameObject hitObject;
 
     public void Start()
     {
         GenerateObject();
     }
 
-    public void Update()
-    {
-        ReGenerateObject(isClickedObject, hitObject);
-    }
-
-    public void GenerateObject()
+    private void GenerateObject()
     {
         Vector3 spawn = new(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), 0);
         Instantiate(purplePointPrefab, spawn, Quaternion.identity);
     }
 
-    public void ReGenerateObject(bool isClickedObject, GameObject hitObject)
+    public void RegenerateObject(bool isClickedObject, GameObject hitObject)
     {
         if (!Input.GetMouseButtonDown(0)) return;
         if (!hitObject) return;
