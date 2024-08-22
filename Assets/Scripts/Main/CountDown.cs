093d1169cdb619eecc069168c9abaf003034c707
134n <goruji.ruji@gmail.com>
2024-08-22 21:54:42 +0900

    Add : カウントダウンの実装
    10秒カウントしたらリザルトへ遷移する

---
+++ Assets/Scripts/Main/CountDown.cs
@@ -0,0 +1,23 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CountDown : MonoBehaviour
+{
+    private float Count = 10;
+    private void Update()
+    {
+        Count -= Time.deltaTime;
+
+        if (Count < 0) ToResult();
+
+        Debug.Log((int)Count);
+    }
+
+    /// <summary>
+    /// リザルトに遷移
+    /// </summary>
+    private void ToResult() => SceneManager.LoadScene("Result");
+}
