6c83bcca022bcb13fbe3142909eaa0100e911c8d
134n <goruji.ruji@gmail.com>
2024-09-16 22:21:24 +0900

    Update : オブジェクトが小さくなっている最中のものがあれば待機する機能を実装

--- Assets/Scripts/Main/CountDown.cs
+++ Assets/Scripts/Main/CountDown.cs
@@ -1,23 +1,25 @@
-using System;
-using TMPro;
-using Unity.VisualScripting;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CountDown : MonoBehaviour
 {
+    public ObjectScaleChange objectScaleChange;
     private float Count = 10;
-    private void Update()
+    private const string loadResultScene = "Result";
+    private void Start()
     {
-        Count -= Time.deltaTime;
+        this.UpdateAsObservable()
+            .Select(_=>  Count -= Time.deltaTime)
+            .Where(_=> Count < 0)
+            .Subscribe(_ =>{
+            
+            Debug.Log(Count);
 
-        if (Count < 0) ToResult();
+            if (objectScaleChange.IsScaleChange) return;
 
-        Debug.Log((int)Count);
+            SceneManager.LoadScene(loadResultScene);
+        });
     }
-
-    /// <summary>
-    /// リザルトに遷移
-    /// </summary>
-    private void ToResult() => SceneManager.LoadScene("Result");
 }
